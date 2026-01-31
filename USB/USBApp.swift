de133516a8b8730f0d4a63289e648404654fff30
gunnarguy <gunnarguy@me.com>
2026-01-30 19:43:54 -0800

    Initial Commit

---
+++ USB/USBApp.swift
@@ -0,0 +1,17 @@
+//
+//  USBApp.swift
+//  USB
+//
+//  Created by Gunnar Hostetler on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct USBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
